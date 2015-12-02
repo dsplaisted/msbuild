99f57113a167f3485ebf54b1ba5d8d58116d0a25
Daniel Plaisted <dsplaisted@gmail.com>
2015-12-02 08:46:41 -0800

    Update entry point build script to use .NET Core MSBuild if MSBUILD_RUNTIME_TYPE is set to "Core"

--- build.cmd
+++ build.cmd
@@ -10,7 +10,20 @@ if not "%VisualStudioVersion%" == "14.0" (
 
 :: Check for a custom MSBuild path. If not defined, default to the one in your path.
 if not defined MSBUILDCUSTOMPATH (
-    set MSBUILDCUSTOMPATH=MSBuild.exe
+    if "%MSBUILD_RUNTIME_TYPE%" == "Core" (
+        set MSBUILDCUSTOMPATH="%~dp0\packages\Microsoft.DotNet.BuildTools\dnxcore50\corerun.exe" "%~dp0\packages\Microsoft.DotNet.BuildTools\dnxcore50\MSBuild.exe"
+    ) else (
+        set MSBUILDCUSTOMPATH=MSBuild.exe
+    )
+) else (
+    set MSBUILDCUSTOMPATH="%MSBUILDCUSTOMPATH"
+)
+
+
+if not "%MSBUILD_RUNTIME_TYPE%" == "Core" (
+    set NODEREUSE=/nodeReuse:false
+) else (
+    set NODEREUSE=
 )
 
 echo ** Restoring build tools
@@ -20,8 +33,8 @@ echo ** MSBuild Path: %MSBUILDCUSTOMPATH%
 echo ** Building all sources
 
 :: Call MSBuild
-echo ** "%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
-"%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
+echo ** %MSBUILDCUSTOMPATH% "%~dp0build.proj" /verbosity:minimal %NODEREUSE% /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
+%MSBUILDCUSTOMPATH% "%~dp0build.proj" /verbosity:minimal %NODEREUSE% /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
 set BUILDERRORLEVEL=%ERRORLEVEL%
 echo.
 
